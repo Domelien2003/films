3b53eefb1934dd6016802c679be23ce7843fe05d
Domelien2003 <zafackdome@gmail.com>
2023-01-25 19:34:50 +0100

    modification

--- js/index.js
+++ js/index.js
@@ -2,20 +2,27 @@
 
 
 
-var bouton=document.getElementById('bouton')
+
 var compteur=document.getElementById('compteur')
 var btn=document.getElementById('btn')
 var bouton=document.querySelectorAll('#bouton')
 
+
 bouton.forEach((item)=>{
     var total=0
+
     item.addEventListener('click', function () {
-        
-        total= total +1
+
+        total= total+1
         document.getElementById("compteur").textContent= total
+
+        document.getElementById("compt").textContent= total
+       
+
     })
 
-    
+   
 })
-    
+
+
 
